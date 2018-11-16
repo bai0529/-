4f9e0b12bd7acf20e4b36322f8f9b8ee7a924414
bai0529 <43664521+bai0529@users.noreply.github.com>
2018-11-16 13:17:52 +0800

    Add files via upload

---
+++ homework/R-百 (1).R
@@ -0,0 +1,27 @@
+---
+  title: "作業"
+author: "高百羚"
+date:  "`r Sys.Date()`"
+output:html_document: default
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+
+##作業一
+
+這是**作業一**的題，下面則是*指令*。
+
+
+```{r}
+1+1
+1+1+2+2+3+3
+```
+
+
+
+```{r ref.label = knitr::all_labels(), echo = TRUE, eval = FALSE}
+
+```
